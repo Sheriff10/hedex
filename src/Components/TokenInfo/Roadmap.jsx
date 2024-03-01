ca71f881ab536872d1270923d6459827b179c459
sheriff ibrahim <ibrahimsheriff999@gmail.com>
2024-03-01 11:16:44 +0100

    add: Roadmap component

--- src/Components/TokenInfo/Roadmap.jsx
+++ src/Components/TokenInfo/Roadmap.jsx
@@ -19,14 +19,14 @@ export default function Roadmap() {
   return (
     <div className="wrap py-16">
       <Container>
-        <div className="heading flex flex-wrap justify-between items-center mb-5">
+        <div className="heading flex flex-wrap justify-center items-center mb-5">
           <span className="lg:text-5xl text-3xl py-4 text-white font-bold text-fierce">
             Hedex <span className="text-grad">Roadmap</span>
           </span>
         </div>
 
         <div className="body">
-          <div className="grid lg:grid-cols-5 gap-10">
+          {/* <div className="grid lg:grid-cols-5 gap-10">
             {roadmap.map((i, index) => (
               <div className="wrap bg-grad pl-2 text-sm" key={index}>
                 <div className="bg-con bg-gray-800  text-gray-300 p-5 px-3 flex gap-5 items-center h-full">
@@ -34,6 +34,9 @@ export default function Roadmap() {
                 </div>
               </div>
             ))}
+          </div> */}
+          <div className="wrap">
+            <img src="/asset/rd.png" alt="Roadmap" className='lg:w-[62%] mx-auto' />
           </div>
         </div>
       </Container>
